449d47ede0afa0eff4d466b964a3115aaf81658e
Emre Uyandiran <emre.uyandiran@hotmail.com>
2022-11-28 21:20:12 +0100

    Die Daten aus OpenWeatherMap.Json wird jetzt in einer Klasse(OpenWeatherMapData) gespeichert.

---
+++ OpenWeatherMapData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aufgabe1
+{
+    class OpenWeatherMapData
+    {
+        public Main main;
+    }
+
+    class Main
+    {
+        public float temp;
+        public float feels_like;
+        public float temp_min;
+        public float temp_max;
+    }
+}
