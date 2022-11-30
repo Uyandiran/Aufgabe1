a2d51cd1ff451dd38ee90f0800fd933377f746ff
Emre Uyandiran <emre.uyandiran@hotmail.com>
2022-11-30 20:04:09 +0100

    Es wurde eine OpenWeatherMap Klasse erstellt, die die Request ausführt.

---
+++ Logging.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Aufgabe1
+{
+    static class Logging
+    {
+        public static void WriteLog(string message)
+        {
+            string logPath = System.Configuration.ConfigurationManager.AppSettings["LogPath"];
+            StreamWriter writer = new StreamWriter(logPath, true);
+            writer.WriteLine($"{DateTime.Now}:" + message);        
+        }
+    }    
+}
+
---
+++ OpenWeatherMap.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+
+namespace Aufgabe1
+{
+    class OpenWeatherMap
+    {
+        public OpenWeatherMap()
+        {
+            requestUri = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=metric";
+            key = "c2b238d710aa8593924a3d04ba80ca97";
+
+            requestUri = requestUri.Replace("{API key}", key);
+            httpResponse = null;
+            response = null;
+            httpClient  = new HttpClient();
+        }
+
+        private string generateURI(string stadt)
+        {
+            return requestUri.Replace("{city name}", stadt);
+        }
+
+        public void openWeatherMapRequest(string stadt)
+        {
+            //Füge den Namen der Stadt in den String hinzu.
+            string str = generateURI(stadt);
+            httpClient = new HttpClient();
+            httpResponse = httpClient.GetAsync(str).Result;
+            string response = httpResponse.Content.ReadAsStringAsync().Result;
+            currentWeatherData = JsonConvert.DeserializeObject<CurrentWeatherData>(response);            
+        }
+
+        public string toString()
+        {
+            str = "Die aktuelle Temperatur beträgt: " + currentWeatherData.main.temp + " grad Celsius, die tiefst Temperatur liegt heute bei : " + 
+                currentWeatherData.main.temp_min + " grad Celsius, die höchst Temperatur bei: "+ currentWeatherData.main.temp_max+" grad Celsius";
+            return str;
+        }
+        
+        private string str;
+        private HttpClient httpClient;
+        private HttpResponseMessage httpResponse;
+        private string key;
+        private string requestUri;
+        private string response;
+        public CurrentWeatherData currentWeatherData;
+    }
+}
